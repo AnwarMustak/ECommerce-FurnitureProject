940c1c40ecf43fdd8f22fc2511c4e084933bc1ba
skanwarmustak@gmail.com <skanwarmustak@gmail.com>
2023-03-29 12:58:27 +0530

    Task Done

---
+++ FurnitureService/src/main/java/com/niit/FurnitureService/domain/Furniture.java
@@ -0,0 +1,18 @@
+package com.niit.FurnitureService.domain;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Document
+public class Furniture {
+    @Id
+    String title;
+    String image;
+    String price;
+}
